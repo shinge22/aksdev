40f5537498ae9d7d4dc4953f06f6b047a756caa7
Sandeep Hinge <sandeep.hinge@gmail.com>
2022-09-14 18:05:53 +0530

    commit

---
+++ shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly ProductContext _context;
+
+        public ProductController(ILogger<ProductController> logger, ProductContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products
+                                    .Find(p => true)
+                                    .ToListAsync();
+        }
+    }
+}
